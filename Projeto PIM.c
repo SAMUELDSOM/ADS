7e79e27fe582c5df11d6505638bba5b3f764f830
SAMUELDSOM <samuelsantosn4@gmail.com>
2022-11-05 16:10:09 -0300

    Versão 0.0.2
    Tela de login e cadastro prontas.

--- Projeto PIM.c
+++ Projeto PIM.c
@@ -9,14 +9,14 @@ Comente sempre que alterar.
 #include <math.h>
 #define SIZE 50
 
-char nome[SIZE][200], cpf[11][200], fone[SIZE][200], linha[SIZE][200], e_mail[SIZE][200], senha[8][200];
+char nome[SIZE][200], cpf[11][200], fone[SIZE][200], linha[SIZE][200], cnpj[14][200], senha[8][200];
 
 void cadastro();
 void login();
 
 int main()
 {
- int cont = 5;
+ int cont = 0;
  int op2 = 0;
  FILE *file;
 
@@ -39,12 +39,12 @@ int main()
              return 1;
             } 
    
-         cadastro(file);/*Chama a função de cadastro de usuário*/ 
+         cadastro(file);//Chama a função de cadastro de usuário 
         
          fclose(file);
          break; 
      
-         /*Tela de login*/
+         //Tela de login
          case 2:
          file = fopen("Usuarios", "r");
             if(file == NULL){
@@ -67,11 +67,32 @@ int main()
             scanf("%s", senha1); 
             system("cls");
 
-            while ((fscanf(file, "%s ; %s\n", login2, senha2)) != EOF)/*Comparando dados do arquivo "Usuarios.txt" com os dados de entrada*/
+            while ((fscanf(file, "%s ; %s\n", login2, senha2)) != EOF)//Comparando dados do arquivo "Usuarios.txt" com os dados de entrada
             {
                 if (strcmp(login, login2) == 0 && strcmp(senha1, senha2)== 0){
                  printf("\n\n\t%s LOGADO!\n\n", login2);
                  login_efetuado = 1;
+                 int sair = 1;
+                    do
+                    {
+                        printf("\n\tMENU INICIAL\n\n");
+                        printf("Cadastrar cliente (digite 1)\n");//Lucas
+                        printf("Cadastrar funcionario (digite 2)\n");//Kaue
+                        printf("");//Samuel
+                        printf("Relatorios(digite 3)\n");//Samuel e Vieira
+                        scanf("%d", &sair);
+
+                        switch (sair)
+                        {
+                         case 1:
+                            /* code */
+                            break;
+                        
+                         default:
+                            break;
+                        }
+                    } while (sair != 0);
+                    
                  /*O restante do codigo será desenvolvido dentro deste IF, pois somente dentro dele
                  é possivel a validação dos dados de login*/
                 }   
@@ -82,16 +103,13 @@ int main()
              printf("CADASTRE-SE OU TENTE NOVAMENTE\n\n");
             }
         
-            
-    
-
          fclose(file);
          break;
         }
     }
  return 0;
 }
-/*Função para cadastro de usuário*/
+//Função para cadastro de usuário
 void cadastro(FILE *fi)
 {
     int opcao;
